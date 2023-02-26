1a914e73b12a04086b3662d745d595fc41516249
MichaelFadyeyev <mihail.fadeev@ukr.net>
2023-02-26 20:17:06 +0200

    Add project files.

---
+++ MyCompany/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepName { get; set; }
+
+        public virtual LinkedList<Employee> Employees { get; set; }
+    }
+}
